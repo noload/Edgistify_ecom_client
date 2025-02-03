bca767d38f5c1d5120ed926cba086a8ff6ed4848
vaibhav kamble <vaibhavkamble023@gmail.com>
2025-02-03 15:45:32 +0530

    added .env file in frontend

--- src/redux/orderAction.jsx
+++ src/redux/orderAction.jsx
@@ -1,34 +1,47 @@
 import axios from "axios";
-import { createOrderFailure, createOrderStart, createOrderSuccess, validatePaymentFailure, validatePaymentStart, validatePaymentSuccess } from "./orderSlice";
-
+import {
+  createOrderFailure,
+  createOrderStart,
+  createOrderSuccess,
+  validatePaymentFailure,
+  validatePaymentStart,
+  validatePaymentSuccess,
+} from "./orderSlice";
+const API_URL = import.meta.env.VITE_API_URL;
 export const createOrder = (orderData, token) => async (dispatch) => {
-    dispatch(createOrderStart());
-    try {
-        const response = await axios.post(
-            "http://localhost:3000/api/order/place-order",
-            orderData,
-            { headers: { Authorization: `Bearer ${token}` } }
-        );
-        dispatch(createOrderSuccess(response.data));
-        return response.data.order;
-    } catch (error) {
-        dispatch(createOrderFailure(error.response?.data || "Failed to place order"));
-        throw error;
-    }
+  dispatch(createOrderStart());
+  try {
+    const response = await axios.post(
+      `${API_URL}/api/order/place-order`,
+      orderData,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    dispatch(createOrderSuccess(response.data));
+    return response.data.order;
+  } catch (error) {
+    dispatch(
+      createOrderFailure(error.response?.data || "Failed to place order")
+    );
+    throw error;
+  }
 };
 
 export const validatePayment = (paymentData, token) => async (dispatch) => {
-    dispatch(validatePaymentStart());
-    try {
-        const response = await axios.post(
-            "http://localhost:3000/api/order/validate-payment",
-            paymentData,
-            { headers: { Authorization: `Bearer ${token}` } }
-        );
-        dispatch(validatePaymentSuccess(response.data));
-        return response.data;
-    } catch (error) {
-        dispatch(validatePaymentFailure(error.response?.data || "Failed to validate payment"));
-        throw error;
-    }
+  dispatch(validatePaymentStart());
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/api/order/validate-payment",
+      paymentData,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    dispatch(validatePaymentSuccess(response.data));
+    return response.data;
+  } catch (error) {
+    dispatch(
+      validatePaymentFailure(
+        error.response?.data || "Failed to validate payment"
+      )
+    );
+    throw error;
+  }
 };
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://edgistify-ecom-server.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const loginUser = async (email, password) => {
   try {
